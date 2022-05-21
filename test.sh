ff4d3d3c111fd01080193bd2eef63e0797b8648e
hjyoon <hjyoon@kakao.com>
2022-05-22 04:07:07 +0900

    Fix test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 for i in $(seq 1 ${4}); do
     res=$(${2} ${3} < ${1}/${i}.in)
     ans=$(cat ${1}/${i}.out)
-    if [ "${res}" == "${ans}" ]; then
+    if [ "${res}" = "${ans}" ]; then
         echo "case #${i}: OK"
     else
         echo "case #${i}: ERR"
